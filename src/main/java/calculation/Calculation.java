923eed486a65d097843871ab3e8b6592d159f1d6
Sudeep-Sharma0-0 <sharma.sudip1122@gmail.com>
2021-07-13 23:22:01 +0545

    Completed Input and Calculations

--- src/main/java/calculation/Calculation.java
+++ src/main/java/calculation/Calculation.java
@@ -1,5 +1,32 @@
 package calculation;
 
-public class Calculation {
+import inputOutput.InputOutput;
 
+public class Calculation {
+    int total = 500, sum, sub = 5;
+    double percent;
+    String grade;
+
+    public void precentage() {
+        sum = ophy + ochem + obio + omath + oeng;
+        percent = sum / total * 100;
+    }
+
+    public void grades() {
+        if (percent > 90) {
+            grade = "A+";
+        } else if (percent > 80 && precent < 90) {
+            grade = "A";
+        } else if (percent > 70 && precent < 80) {
+            grade = "B+";
+        } else if (percent > 60 && precent < 70) {
+            grade = "B";
+        } else if (percent > 50 && precent < 60) {
+            grade = "C+";
+        } else if (percent > 40 && precent < 50) {
+            grade = "C";
+        } else {
+            grade = "F";
+        }
+    }
 }
--- src/main/java/inputOutput/InputOutput.java
+++ src/main/java/inputOutput/InputOutput.java
@@ -3,12 +3,14 @@ package inputOutput;
 import java.util.Scanner;
 
 public class InputOutput {
+    //Initialization of data members and methods....
     String name, fname, symbol;
-    int id, phy, chem, bio, math;
-
+    int id, ophy = -1, ochem = -1, obio = -1, omath = -1, oeng = -1;
     Scanner input = new Scanner(System.in);
 
+    //User input Section.....
     public void input() {
+        //Personal Details.....
         System.out.println("Enter your name: ");
         name = input.nextLine();
 
@@ -17,6 +19,59 @@ public class InputOutput {
 
         System.out.println("Enter your id: ");
         id = input.nextInt();
+
+        System.out.println("Enter your father's name: ");
+        fname = input.nextLine();
+        input.nextLine();
+
+        //Marks section........
+        System.err.println("\nThe full marks is 100....\n");
+
+        //Physics.....
+        while (ophy < 0 || ophy > 100) {
+            System.out.println("Enter obtained marks of Physics: ");
+            ophy = input.nextInt();
+            validity(ophy);
+        }
+
+        //Chemestry.....
+        while (ochem < 0 || ochem > 100) {
+            System.out.println("Enter obtained marks of Chemestry: ");
+            ochem = input.nextInt();
+            validity(ochem);
+        }
+
+        //Biology....
+        while (obio < 0 || obio > 100) {
+            System.out.println("Enter obtained marks of Biology: ");
+            obio = input.nextInt();
+            validity(obio);
+        }
+
+        //Mathematics....
+        while (omath < 0 || omath > 100) {
+            System.out.println("Enter obtained marks of Mathematics: ");
+            omath = input.nextInt();
+            validity(omath);
+        }
+
+        //English.....
+        while (oeng < 0 || oeng > 100) {
+            System.out.println("Enter obtained marks of English: ");
+            oeng = input.nextInt();
+            validity(oeng);
+        }
+    }
+
+    //Check if the input marks are validate....
+    public void validity(int v) {
+        if (v < 0 || v > 100) {
+            System.err.println("The obtained marks should be within 0-100!!!");
+        }
+    }
+
+    //Output Section.....
+    public void output() {
 
     }
 }
